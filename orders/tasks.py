3c9d9309e2141872d102d94944e1bbbd125027ce
Rogerio Oliveira <rgrlinux@gmail.com>
2023-10-03 00:00:57 -0300

    rename original task, add task and function to open order page

--- orders/tasks.py
+++ orders/tasks.py
@@ -1,6 +1,21 @@
+
 from robocorp.tasks import task
+from robocorp import browser
+
 
 @task
-def minimal_task():
-    message = "Hello"
-    message = message + " World!"
+def order_robots_from_RobotSpareBin():
+    """
+    Orders robots from RobotSpareBin Industries Inc.
+    Saves the order HTML receipt as a PDF file.
+    Saves the screenshot of the ordered robot.
+    Embeds the screenshot of the robot to the PDF receipt.
+    Creates ZIP archive of the receipts and the images.
+    """
+    browser.configure(slowmo=1000)
+    open_robot_order_wbesite()
+
+
+def open_robot_order_wbesite():
+    """Navegate to order page"""
+    browser.goto('https://robotsparebinindustries.com/#/robot-order')
