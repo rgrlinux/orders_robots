9e8cfda30c289d3c54eb9c88313aab1a686eec40
Rogerio Oliveira <rgrlinux@gmail.com>
2023-10-03 20:16:44 -0300

    remove licence duplicated

--- orders/tasks.py
+++ orders/tasks.py
@@ -1,6 +1,7 @@
-
+import os
 from robocorp.tasks import task
 from robocorp import browser
+from RPA.HTTP import HTTP
 
 
 @task
@@ -15,6 +16,7 @@ def order_robots_from_RobotSpareBin():
     browser.configure(slowmo=1000)
     open_robot_order_website()
     close_annoying_modal()
+    download_csv_file()
 
 
 def open_robot_order_website():
@@ -25,3 +27,10 @@ def open_robot_order_website():
 def close_annoying_modal():
     page = browser.page()
     page.click('text=OK')
+
+
+def download_csv_file():
+    """Dowload csv file from http"""
+    http = HTTP()
+    http.download('https://robotsparebinindustries.com/orders.csv',
+                  os.getcwd(), overwrite=True)
