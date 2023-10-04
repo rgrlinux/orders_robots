1bbd62de212100cf6c19e45d8cc0f21f875689a6
Rogerio Oliveira <rgrlinux@gmail.com>
2023-10-03 21:27:48 -0300

    add get_orders, list_and_send_ordersand fill_form_orders

--- orders/tasks.py
+++ orders/tasks.py
@@ -2,6 +2,7 @@ import os
 from robocorp.tasks import task
 from robocorp import browser
 from RPA.HTTP import HTTP
+from RPA.Tables import Tables
 
 
 @task
@@ -17,6 +18,7 @@ def order_robots_from_RobotSpareBin():
     open_robot_order_website()
     close_annoying_modal()
     download_csv_file()
+    list_and_send_orders()
 
 
 def open_robot_order_website():
@@ -34,3 +36,28 @@ def download_csv_file():
     http = HTTP()
     http.download('https://robotsparebinindustries.com/orders.csv',
                   os.getcwd(), overwrite=True)
+
+
+def get_orders():
+    """Load csv into table"""
+    tables = Tables()
+    table_data = tables.read_table_from_csv('orders.csv')
+    return table_data
+
+
+def list_and_send_orders():
+    """List orders ans send each to fill form"""
+    orders = get_orders()
+    for order in orders:
+        fill_form_and_order(order)
+
+
+def fill_form_and_order(order):
+    """Receive one order and fill form"""
+    page = browser.page()
+    page.select_option('#head', str(order['Head']))
+    body = f'#id-body-{str(order["Body"])}'
+    page.set_checked(body, True)
+    page.fill(selector='//input[@type="number"]', value=str(order['Legs']))
+    page.fill('#address', order['Address'])
+    page.click('text=Order')
